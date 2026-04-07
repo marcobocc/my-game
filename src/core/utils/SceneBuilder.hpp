3f110e05b604c57abd969062288a64e1866c0742
marcobocc <marcobocchetti3@gmail.com>
2026-04-07 13:17:44 +0100

    feat(core): add method in SceneBuilder to create cameras

--- src/core/utils/SceneBuilder.hpp
+++ src/core/utils/SceneBuilder.hpp
@@ -1,9 +1,9 @@
 #pragma once
-#include <glm/glm.hpp>
 #include <string>
 #include "GameEngine.hpp"
 #include "MaterialBuilder.hpp"
 #include "PrimitiveBuilder.hpp"
+#include "core/components/CameraComponent.hpp"
 #include "core/components/RenderableComponent.hpp"
 #include "core/components/TransformComponent.hpp"
 
@@ -39,6 +39,28 @@ public:
         unsigned int entityId = engine_.getECS().createEntity();
         engine_.getECS().addComponent(entityId, std::move(createTransform(position, scale)));
         engine_.getECS().addComponent(entityId, std::move(renderableComponent));
+        return entityId;
+    }
+
+    unsigned int createCamera(const glm::vec3& position = {0.0f, 0.0f, 0.0f},
+                              const glm::vec3& forward = {0.0f, 0.0f, -1.0f},
+                              const glm::vec3& up = {0.0f, 1.0f, 0.0f},
+                              float fov = 45.0f,
+                              float aspect = 16.0f / 9.0f,
+                              float nearPlane = 0.1f,
+                              float farPlane = 100.0f) const {
+
+        CameraComponent camera{};
+        camera.position = position;
+        camera.forward = forward;
+        camera.up = up;
+        camera.fov = fov;
+        camera.aspect = aspect;
+        camera.nearPlane = nearPlane;
+        camera.farPlane = farPlane;
+
+        unsigned int entityId = engine_.getECS().createEntity();
+        engine_.getECS().addComponent(entityId, std::move(camera));
         return entityId;
     }
 
