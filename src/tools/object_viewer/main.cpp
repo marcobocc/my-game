e483e3d4b9072b8723415b70d12a1ce11c25a0d5
marcobocc <marcobocchetti3@gmail.com>
2026-04-09 22:04:23 +0100

    fix(tools): logging configuration

--- src/tools/object_viewer/main.cpp
+++ src/tools/object_viewer/main.cpp
@@ -1,6 +1,15 @@
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/consoleappender.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/patternlayout.h>
 #include "ObjectViewerApp.hpp"
 
 int main() {
+    const log4cxx::LayoutPtr layout(new log4cxx::PatternLayout("%d [%t] %-5p %c (%F:%L) - %m%n"));
+    const log4cxx::AppenderPtr console(new log4cxx::ConsoleAppender(layout));
+    log4cxx::LoggerPtr logger(log4cxx::Logger::getRootLogger());
+    log4cxx::Logger::getRootLogger()->addAppender(console);
+
     constexpr unsigned int width = 1280;
     constexpr unsigned int height = 720;
     ObjectViewerApp app(width, height);
